70e0d1cead457758ba8178dc140d3b3ecae496ea
Kunal Sarkar <kunalsarkar91@gmail.com>
2016-10-08 22:39:10 +0530

    Basic React working

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log("Hi!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello World!</h1>
+  }
+}
+ 
+ReactDOM.render(<Hello/>, document.getElementById('helloworld'));
